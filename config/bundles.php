a4200ddcf1ea62212d237e9cd709af87cd77e7a8
Brin Amonkonan <brin.amonkonan@gmail.com>
2023-08-30 04:40:35 +0200

    lexik config login step 1

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+
 ];
